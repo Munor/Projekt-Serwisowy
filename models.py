1930981c23f8e4859cb3311580434ade0d4185e3
Kacper Hałaczkiewicz <54312917+ikarmus2001@users.noreply.github.com>
2025-04-06 10:04:45 +0200

    Poprawienie metod http, przeniesienie instancjonowania SQLAlchemy do modelu (circular import)

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-from service_manager import db 
+# from service_manager import db 
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 class ServiceRequest(db.Model):
     __tablename__ = 'requests'
     
